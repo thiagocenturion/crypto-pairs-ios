1b49ad6116c3a44199f48d6a4f3f888fb02e9430
Thiago Centurion <thiagocenturion@me.com>
2022-06-10 15:08:54 +0100

    Create Xcode project

---
+++ CryptoPairs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoPairs
+//
+//  Created by Thiago Centurion on 10/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CryptoPairsUITests/CryptoPairsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CryptoPairsUITestsLaunchTests.swift
+//  CryptoPairsUITests
+//
+//  Created by Thiago Centurion on 10/06/2022.
+//
+
+import XCTest
+
+class CryptoPairsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
